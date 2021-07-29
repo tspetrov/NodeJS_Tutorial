96472bf794ede9bea520d6087ef6eef09ad67fe7
Tsvetomir <petrov.tsvetomir@vistajet.com>
2021-07-29 10:23:51 +0300

    Node and express tutorial examples

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-const person = require('./person');
-console.log(person);
-console.log(person.name);
+// Module wrapper function - you have this available in a module logic
+// (function (exports, require, module, __filename, __dirname) {
+//})
 
-const personFromClass = require('./personClass');
-const person1 =  new personFromClass('Ceco', 15);
-person1.greeting();
+// const person = require('./person'); // method of importing called common JS
+// //import Person from './person'; = Im ES6 method, but still not included in node
+// console.log(person);
+// console.log(person.name);
+//
+// const personFromClass = require('./personClass');
+// const person1 =  new personFromClass('Ceco', 15);
+// person1.greeting();
+//
+//
+// console.log("Hello from Node.js");
 
+const express = require('express');
 
-console.log("Hello from Node.js");
+const app = express();
+
+const port = 5609;
+app.listen(port, () => console.log(`Server started on port = ${port}`));
---
+++ reference/os_demo.js
@@ -0,0 +1,19 @@
+const os = require('os');
+
+//Platform
+console.log(os.platform());
+
+// CPU Arch
+console.log(os.arch());
+
+// CPU Core info
+console.log(os.cpus());
+
+// Free memory
+console.log(os.freemem());
+
+// Home directory
+console.log(os.homedir());
+
+// Uptime
+console.log(os.uptime()/60/60);
